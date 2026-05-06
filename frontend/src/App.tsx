e68cca709b89f594a6d580c890b74fcdab37e0f4
taserbeat <taserbeat@gmail.com>
2026-05-06 21:06:12 +0900

    フロントエンドのプロジェクトを作成

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>{count}</p>
+
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        +1
+      </button>
+
+      <button
+        onClick={() => {
+          setCount((prev) => prev - 1);
+        }}
+      >
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default App;
