7146398b002071448cf01f63c6dd7b851bd92c55
taserbeat <taserbeat@gmail.com>
2026-05-16 22:51:51 +0900

    refactor: ユーザー更新リクエストのJsonプロパティ名を明示的に指定

--- backend/Web/Dtos/Users/UpdateUser/UpdateUserRequest.cs
+++ backend/Web/Dtos/Users/UpdateUser/UpdateUserRequest.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Web.Dtos.Users.UpdateUser
 {
     /// <summary>
@@ -9,36 +11,42 @@ namespace Web.Dtos.Users.UpdateUser
         /// メールアドレス
         /// </summary>
         /// <value></value>
+        [JsonPropertyName("email")]
         public string? Email { get; set; }
 
         /// <summary>
         /// パスワード
         /// </summary>
         /// <value></value>
+        [JsonPropertyName("password")]
         public string? Password { get; set; }
 
         /// <summary>
         /// 姓
         /// </summary>
         /// <value></value>
+        [JsonPropertyName("lastName")]
         public string? LastName { get; init; }
 
         /// <summary>
         /// 名
         /// </summary>
         /// <value></value>
+        [JsonPropertyName("firstName")]
         public string? FirstName { get; init; }
 
         /// <summary>
         /// ロールID
         /// </summary>
         /// <value></value>
+        [JsonPropertyName("roleId")]
         public Guid? RoleId { get; init; }
 
         /// <summary>
         /// 有効フラグ
         /// </summary>
         /// <value></value>
+        [JsonPropertyName("isActive")]
         public bool? IsActive { get; init; }
     }
 }
