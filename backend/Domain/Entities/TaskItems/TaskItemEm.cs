682d2642d774fad75bb38313e66714a4f2722252
taserbeat <taserbeat@gmail.com>
2026-05-19 23:01:36 +0900

    feat: タスクのエンティティモデルにドメイン操作のメソッドを実装

--- backend/Domain/Entities/TaskItems/TaskItemEm.cs
+++ backend/Domain/Entities/TaskItems/TaskItemEm.cs
@@ -1,5 +1,6 @@
 using Domain.Entities.BoardColumns;
 using Domain.Entities.Common;
+using Domain.Entities.Tenants;
 using Domain.Entities.Users;
 
 namespace Domain.Entities.TaskItems
@@ -67,7 +68,105 @@ namespace Domain.Entities.TaskItems
         /// 作成者のナビゲーションプロパティ
         /// </summary>
         /// <value></value>
-        public UserEm? Creator { get; private set; } = default!;
+        public UserEm? Creator { get; private set; } = default;
+
+        public static TaskItemEm Create(
+            TaskItemId id,
+            TenantId tenantId,
+            BoardColumnId boardColumnId,
+            UserId? assigneeId,
+            TaskItemTitle title,
+            TaskItemDescription description,
+            TaskItemPriorityEnum priority,
+            DateTimeOffset? dueDate,
+            TaskItemPosition position,
+            DateTimeOffset createdAt,
+            DateTimeOffset updatedAt,
+            UserId? createdBy,
+            UserId? updatedBy
+        )
+        {
+            return new TaskItemEm()
+            {
+                Id = id,
+                TenantId = tenantId,
+                CreatedAt = createdAt,
+                UpdatedAt = updatedAt,
+                CreatedBy = createdBy,
+                UpdatedBy = updatedBy,
+                BoardColumnId = boardColumnId,
+                AssigneeId = assigneeId,
+                Title = title,
+                Description = description,
+                Priority = priority,
+                DueDate = dueDate,
+                Position = position,
+            };
+        }
+
+        public void ChangeColumn(BoardColumnId newColumnId, TaskItemPosition newPosition, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            BoardColumnId = newColumnId;
+            Position = newPosition;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void Assign(UserId newAssigneeId, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            AssigneeId = newAssigneeId;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void ReleaseAssignee(DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            AssigneeId = null;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void ChangeTitle(TaskItemTitle newTitle, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            Title = newTitle;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void ChangeDescription(TaskItemDescription newDescription, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            Description = newDescription;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void ChangePriority(TaskItemPriorityEnum newPriority, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            Priority = newPriority;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void ChangeDueDate(DateTimeOffset newDueDate, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            DueDate = newDueDate;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void DeleteDueDate(DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            DueDate = null;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
+
+        public void ChangePosition(TaskItemPosition newPosition, DateTimeOffset updatedAt, UserId? updatedBy)
+        {
+            Position = newPosition;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
+        }
     }
 
     /// <summary>
